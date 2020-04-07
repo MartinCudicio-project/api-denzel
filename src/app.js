f112571c6981b571d0f13781da649f1762fa1e0c
martincudicio <martin.cudicio@edu.devinci.fr>
2020-04-08 00:11:27 +0200

    rectify app.use(router)

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(cors({
 }));
 app.use(bodyParser.json())
 // app.use('/movies', moviesRoute)
-app.use('/movies',moviesRoute)
+app.use('/.netlify/functions/app',moviesRoute)
 connectDB();
 
 
